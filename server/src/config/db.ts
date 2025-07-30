4135c2e549fd36534b71dc6030850e28a2c9eb19
samlik shan <samlikshanm@gmail.com>
2025-07-30 12:19:45 +0530

    initial commit

---
+++ server/src/config/db.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI as string);
+    console.log(`MongoDB connected`);
+  } catch (error: any) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1); 
+  }
+};
---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import { connectDB } from "./config/db";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+connectDB()
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
